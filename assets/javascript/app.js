a02e587e30fbe6c2fb4d013fe1d0a9130c40fec3
Lyly <yhl4340@gmail.com>
2019-01-12 13:08:37 -0800

    list/buttons working now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,77 @@
-
-
 //globals
 var wins = 0;
 var losses = 0;
 var score = 0;
 var timeRunning = 30;
 var currentQ = 0;
-var input = '';
-//setting the questions as an array 
+var input = "";
+//setting the questions as an array
 var questions = [
-    {
-        title: '*The character of this movie enters the Land of the  Dead to seek blessing from his grandfather? ',
-        answer: ['Finding Nemo', 'Moana','Coco', 'Wall E'],
-        correct: 2
-        
-    },
-    {
-        title: 'This dad sets out on a journey to bring his son home after his son is taken from the Great Reef to Sydney.' ,
-        answer: ['Coco','Finding Nemo', 'Up','Zootopia'],
-        correct:1
-     }];
-    
+  {
+    title:
+      "*The character of this movie enters the Land of the  Dead to seek blessing from his grandfather? ",
+    answer: ["Finding Nemo", "Moana", "Coco", "Wall E"],
+    correct: 2
+  },
+  {
+    title:
+      "This dad sets out on a journey to bring his son home after his son is taken from the Great Reef to Sydney.",
+    answer: ["Coco", "Finding Nemo", "Up", "Zootopia"],
+    correct: 1
+  }
+];
 
-
-window.onload = function () {
-    showQ();
-  
-}
+window.onload = function() {
+  showQ();
+  $("#question button").on("click", function() {
+    $(this).addClass("clicked");
+    console.log($(this).attr("class"));
+    var guess = $(this).text();
+    $(this).attr("data-val", question.correct);
+    console.log(this);
+    console.log(guess);
+  });
+};
 
 //--------------------functions-----------------------
 //randomly pick a title from array and inserts the answers into the li
 function showQ() {
-    var question = questions[Math.floor(Math.random() * questions.length)];
-    $('#question ul').hide();
-    $('#question').html(question.title);
-    console.log(question.title);
+  var question = questions[Math.floor(Math.random() * questions.length)];
+  $("#question ul").hide();
+  $("#question").html(question.title);
+  console.log(question.title);
 
-//for loop to loop through the answer property of the chosen array. then append them to the li
-    for (var i = 0; i < question.answer.length; i++){
-        var newLi = $('<button>' + question.answer[i] + '</button>' ); 
-        $('#question').append(newLi);
-        console.log(question.answer[i]);
-    }    
-};
-//setting up onclick function when user clicks on the choices
-$('#question').on('click','button', function() {
-   $('<button>').addClass('active');
-
-    $('<button>').submit()
-    
-});
-
-
-function checkAnswer() {
-//  if (questions.correct === )   
-
+  //for loop to loop through the answer property of the chosen array. then append them to the li
+  for (var i = 0; i < question.answer.length; i++) {
+    var newLi = $(
+      '<button id=" ' + i + '">' + question.answer[i] + "</button>"
+    );
+    $("#question").append(newLi);
+    console.log(question.answer[i]);
+  }
 }
-function summary () {
 
+// //setting up onclick function when user clicks on the choices
+// $('#question button').on('click',  function() {
+//    $(this).addClass('clicked');
+//    console.log($(this).attr('class'));
+//    var guess = $('button');
+//    guess.addClass('clicked');
+//    guess.attr('data-val', question.correct);
+
+//    console.log(guess.attr('data-val'));
+// });
+
+function checkAnswer(guess) {
+  if (questions.correct == guess) {
+    alert("yes!");
+    console.log(guess);
+    wins++;
+    $("#wins").html("Wins: " + wins);
+  }
 }
+
+function summary() {}
 //set the timers for 10 sec and the time's up
 
 //  ==========================================instructions======================
@@ -69,4 +82,4 @@ function summary () {
 // --If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
 // If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
 
-// --On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page). 
+// --On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
