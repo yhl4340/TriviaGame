0147126100ddd8697c759177f41a119c233f3398
Lyly <yhl4340@gmail.com>
2019-01-15 16:11:49 -0800

    added fx stop.got currentq to work for the correct answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,11 @@
 // globals
-var correct= 0;
+var correct = 0;
 var incorrect = 0;
 var score = 0;
 var timeRunning = 10;
 var currentQ = 0;
 var intervalId;
-
+var clockRunning = false;
 // setting the questions as an array
 var questions = [
   {
@@ -13,37 +13,33 @@ var questions = [
       "*The character of this movie enters the Land of the  Dead to seek blessing from his grandfather? ",
     answer: ["Finding Nemo", "Moana", "Coco", "Wall E"],
     correct: 2,
-    // imageRight: 'assets/images/giphy.gif',
-    // imageWrong: 'assets/images/test(1).gif'
-   },
+    imageRight: "assets/images/giphy.gif",
+    imageWrong: "assets/images/test(1).gif"
+  },
   {
     title:
       "This dad sets out on a journey to bring his son home after his son is taken from the Great Reef to Sydney.",
     answer: ["Coco", "Finding Nemo", "Up", "Zootopia"],
     correct: 1,
-    //  imageFile: 'assets/images/nemo1.gif',
-    // imageWrong:  'assets/images/Nemo.gif'
+    imageRight: "assets/images/nemo1.gif",
+    imageWrong: "assets/images/Nemo.gif"
   }
 ];
-var imageRight= [
- "<img src = 'assets/images/nemo1.gif'>",
+var imageRight = [
+  "<img src = 'assets/images/nemo1.gif'>",
   "<img src = 'assets/images/giphy.gif'>"
 ];
-var imageWrong = [
-  'assets/images/Nemo.gif',
-  'assets/images/test(1).gif'
-];
+var imageWrong = ["assets/images/Nemo.gif", "assets/images/test(1).gif"];
 
 window.onload = function() {
   $("#summary").hide();
   $("#reset").hide();
   showQ();
-  $('#question button').on('click', run);
-    $("#question button").on("click", function() {
+  $("#question button").on("click", run);
+  $("#question button").on("click", function() {
     $(this).addClass("clicked");
-      console.log($(this).attr("class"));
-      
-    });
+    console.log($(this).attr("class"));
+  });
 };
 
 //--------------------functions-----------------------
@@ -58,84 +54,88 @@ function showQ() {
   // for loop to loop through the answer property of the chosen array. then append them to the button
   for (var i = 0; i < question.answer.length; i++) {
     // creating new button with an id that i can use later.
-    var newLi = $('<button id="' + i + '">' + question.answer[i] + "</button>"
-    );
+    var newLi = $('<button id="' + i + '">' + question.answer[i] + "</button>");
     // attaching the newli to the div w a question id
     $("#question").append(newLi);
     console.log(question.answer[i]);
   }
-};
+}
 
 // setting up the submit function
-$('.btn').on('click',  function() {
-    
+$(".btn").on("click", function() {
   // getting the id value of the button clicked via JQ attr
-  // turning the str value into interger 
-  var userGuess = parseInt($('button.clicked').attr('id'));
-  console.log(userGuess + ' line58');
+  // turning the str value into interger
+  var userGuess = parseInt($("button.clicked").attr("id"));
+  console.log(userGuess + " line58");
   // calling the check function to compare result
-  check(userGuess); 
+  check(userGuess);
   decrement();
-  });
+
+});
+
 
 //----------------------function to check guess
-  function check(userGuess){
-     
-    var question = questions[currentQ];
-    console.log(question.correct);
-    var isCorrect = false;
-    var imgRight = imageRight[Math.floor(Math.random() * imageRight.length)];
-    if(userGuess ==  question.correct) {
-      isCorrect = true;
-      correct++;
-      $('#correct').html('Correct: ' + correct);
-      $('img #img-right').show(imgRight);
-      return 'yay';
-  }else{
+function check(userGuess) {
+  var question = questions[currentQ];
+  console.log(question.correct);
+ 
+  var imgRight = imageRight[Math.floor(Math.random() * imageRight.length)];
+  if (userGuess == question.correct) {
+    correct++;
+    currentQ++;
+    showQ();
+
+    $("#img-right").html(imgRight);
+    return "yay";
+  } else {
     incorrect++;
-    $("#incorrect").html('Incorrect: ' + incorrect);
-    
+   
   };
-    
-//increment currenq outside of the if function becase the count continues whether or not if the user gets it right or not.
-//if run out of questions, then show summary, otherwise, shown next question
+
+
+  //increment currenq outside of the if function becase the count continues whether or not if the user gets it right or not.
+  //if run out of questions, then show summary, otherwise, shown next question
   currentQ++;
-  if (currentQ > questions.length){
-    summary();
-  }else{
-  showQ();
-  }
-};
+  // if (currentQ >= questions.length) {
+  //   stop();
+  //   summary();
+  // } else {
+    showQ();
+  };
+  //================question. i had to put currentq and show q in the if condition for the next q to load. why???????????
+
 
 //------------------------function for timer
 // the run function sets the timer interval at 1 second decrement
-function run(){
-  intervalId= setInterval(decrement,1000)
-};
-// the decrement function decreses timeRunning by 1 interval and prints to dom
-function decrement(){
-  timeRunning--;
-  $('#time-remaining').html('Time remaining: ' + timeRunning);
-};
-
-
-
-
-function summary() {}
-$("#wapper").hide();
-//set the timers for 10 sec and the time's up
-
-if(timeRunning=== 0){
-  if(correct > incorrect){
-    alert ('Awesome!');
-  }else {
-    alert ('Try again');
-  }
+function run() {
+  intervalId = setInterval(decrement, 1000);
+  clockRunning= true;
 }
-$('#correct').html(correct);
-$('#incorrect').html(incorrect);
-$('#total').html('You scored '+ correct + 'out of '+ 5);
+// the decrement function decreses timeRunning by 1 interval and prints to dom
+function decrement() {
+  timeRunning--;
+  $("#time-remaining").html("Time remaining: " + timeRunning);
+}
+function stop(){
+  clearInterval(intervalId);
+  clockRunning= false;
+};
 
+
+//----------------------------------------------------
+function summary() {
+  $("#wapper").hide();
+  //set the timers for 10 sec and the time's up
+  if (correct > incorrect) {
+    alert("Awesome!");
+  } else {
+    alert("Try again");
+  };
+
+  $("#correct").text(correct);
+  $("#incorrect").text(incorrect);
+  $("#total").html("You scored " + correct + "out of " + questions.length);
+};
 
 //  ==========================================instructions======================
 
